884204ae2541bcaca8e3ce8239da51163561fb8a
Theodore Stassinos <thestas@yahoo.com>
2022-11-03 15:42:19 +0200

    Add project files.

---
+++ DataVerse/Models/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataVerse.Models
+{
+    public class CustomerViewModel
+    {
+       public List<Customer> Customers { get; set; }
+       public Customer ActiveCustomer { get; set; }
+    }
+}
